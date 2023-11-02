b27b8b866607954e271d8690a95cf0007b9be033
Alexander Bogdashkin <a_bogdashkin@mail.ru>
2023-11-02 18:20:39 +0300

    add homework

--- src/js/move.js
+++ src/js/move.js
@@ -1,15 +1,16 @@
 export default class Move {
   randomMove() {
     const activeElement = document.querySelector(".active");
-
+    const activeImg = document.querySelector(".img");
+    const imgElement = document.createElement("img");
+    imgElement.src = 'https://raw.githubusercontent.com/netology-code/ahj-homeworks/video/dom/pic/goblin.png';
     const itemArray = Array.from(document.querySelectorAll(".square"));
-
     const randomItem = Math.floor(Math.random() * itemArray.length);
 
     activeElement.classList.remove("active");
+    activeElement.firstElementChild.remove();
 
     itemArray[randomItem].classList.add("active");
-    // console.log(activeElement);
-    // console.log(itemArray);
+    itemArray[randomItem].append(imgElement);
   }
 }
